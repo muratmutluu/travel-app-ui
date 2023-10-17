9133a814b09561aefd6882e9f43adce1454a28a0
Murat Mutlu <muratmutlu010@gmail.com>
2023-10-18 01:42:11 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import Camp from '@/components/Camp';
+import Features from '@/components/Features';
+import GetApp from '@/components/GetApp';
+import Guide from '@/components/Guide';
+import Hero from '@/components/Hero';
 
 const Home = () => {
-  return <h1 className="font bold text-teal-500 text-2xl">Hello</h1>;
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
+  );
 };
 
 export default Home;
